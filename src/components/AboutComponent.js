c03e9df7a03c571218e75ce60e2c875dd4ef00ba
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2021-01-07 12:57:19 +0600

    Review other assignment

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,32 +1,35 @@
+
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-function RenderLeader({leader}) {
-    return (
-        <div key={leader.id} className="col-12 mt-5">
-            <Media tag="li">
-                <Media left middle>
-                    <Media object src={leader.image} alt={leader.name} />
-                </Media>
-                <Media body className="ml-5">
-                    <Media heading>{leader.name}</Media>
-                    <p>{leader.designation}</p>
-                    <p>{leader.description}</p>
-                </Media>
-            </Media>
-        </div>
-    );
-}
 
+function RenderLeader({leader}){
+    return (
+        <Media className="col-12 mt-5">
+          <Media left className="mt-1 m-2">
+            <Media object src={leader.image} alt={leader.name} />
+          </Media>
+          <Media body className="ml-5">
+            <Media heading>
+              {leader.name}
+            </Media>
+            {leader.designation}<br></br>
+            {leader.description}
+          </Media>
+        </Media>
+      );
+
+}
 function About(props) {
 
     const leaders = props.leaders.map((leader) => {
         return (
-            <RenderLeader leader={leader} />
+            <div>
+           <RenderLeader leader={leader}/>
+           </div>
         );
     });
-
     return(
         <div className="container">
             <div className="row">
