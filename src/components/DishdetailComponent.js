e90799f46f3e08db5f68a26b6179b710bf69d51f
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2021-01-12 12:16:55 +0600

    Redux Actions to add comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -20,7 +20,7 @@ function RenderDish({ dish }) {
   }
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, dishId }) {
   console.log(comments);
   if (comments != null) {
     let list = comments.map((comment) => {
@@ -45,7 +45,7 @@ function RenderComments({ comments }) {
       <div>
         <h4>Comments</h4>
         <ul className="list-unstyled">{list}</ul>
-        <CommentForm></CommentForm>
+        <CommentForm  dishId={dishId} addComment={addComment} />
       </div>
     );
   } else {
@@ -74,7 +74,9 @@ const Dishdetail = (props) => {
           <RenderDish dish={props.dish} />
         </div>
         <div className="col-12 col-md-5 m-1">
-          <RenderComments comments={props.comments} />
+          <RenderComments comments={props.comments} 
+            addComment={props.addComment}
+            dishId={props.dish.id} />
         </div>
       </div>
     </div>
@@ -106,9 +108,7 @@ class CommentForm extends Component {
   handleSubmit(values) {
 
       this.toggleModal();
-
-      console.log('Current State is: ' + JSON.stringify(values));
-      alert('Current State is: ' + JSON.stringify(values));
+      this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
   }
 
   render() {
